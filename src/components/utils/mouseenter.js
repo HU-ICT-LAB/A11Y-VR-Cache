f9f0233a43678d03f229aff03a1466a1c126b1c3
niels <niels.hoogeboom@student.hu.nl>
2023-04-05 12:31:36 +0200

    collision effect toegevoegd

--- src/components/utils/mouseenter.js
+++ src/components/utils/mouseenter.js
@@ -1,17 +1,10 @@
-AFRAME.registerComponent('scale-on-mouseenter', {
-    schema: {
-        to: {default: '10 10 10', type: 'vec3'},
-        back: {default: '2 2 2', type: 'vec3'}
-    },
-
-    init: function () {
-         var data = this.data;
+AFRAME.registerComponent('border-collide', {
+    
+    init: function() {
         var el = this.el;
-        this.el.addEventListener('click', function () {
-            if(el.object3D.scale === data.to.default){
-                el.object3D.scale.copy(data.back);
-            }
-            el.object3D.scale.copy(data.to);
-    });
-}
+
+        el.addEventListener('collide', () => {
+            document.getElementById("right").components.haptics.pulse(1, 50);
+        })
+    }
 })
