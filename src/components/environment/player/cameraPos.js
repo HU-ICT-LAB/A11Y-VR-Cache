a64b75d618a30d3293e63cf10088f12395a2d90f
Julie Gunning <juulgunning@gmail.com>
2023-04-13 15:59:02 +0200

    camera test

--- src/components/environment/player/cameraPos.js
+++ src/components/environment/player/cameraPos.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent("cameraPos", {
 	init: function () {
 		const element = this.el;
         element.addEventListener("enter-vr", () => {
-            document.getElementById("camera").setAttribute("position", "0 1 0");
+            document.getElementById("camera").setAttribute("position", "0 0 0");
+        
         })
 	}
 })
