eda809fb1f388072c2174d2eb012b1f29333a724
Julie Gunning <juulgunning@gmail.com>
2023-05-01 11:02:07 +0200

    stoel asset geplaatst

--- src/components/environment/world/chair.js
+++ src/components/environment/world/chair.js
@@ -3,7 +3,7 @@ AFRAME.registerComponent("chair", {
 	init: function () {
         const el = this.el;
         el.setAttribute("class", "interactable");
-        el.addEventListener("raycast-intersected", () => {
+        el.addEventListener("raycaster-intersected", () => {
 			console.log("stoel");
             document.getElementById("chairSound").components.sound.playSound();
 		});
