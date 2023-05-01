0ee7f30f7a7164b1e3fed8cdd5d445128ddcfc40
niels_desktop <niels.hoogeboom@student.hu.nl>
2023-05-01 20:14:02 +0200

    geluiden toegevoegd voor staat kast open en kast dicht

--- src/components/environment/world/Closet.js
+++ src/components/environment/world/Closet.js
@@ -1,22 +1,23 @@
 AFRAME.registerComponent("own-closet", {
 	init: function () {
         this.el.setAttribute("class", "interactable");
+        this.el.setAttribute("sound", "src: #kast;")
 
         this.el.addEventListener("raycaster-intersected", () => {
+            this.el.components.sound.playSound();
+        })
+        this.el.addEventListener("click", () => {
             console.log("ik ben een coole kast");
-            this.el.setAttribute("animation-mixer", "clip: L_door|open_left; timeScale: 1"); 
+            this.el.setAttribute("animation-mixer", "clip: 001; timeScale: 0.2;");
+            setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1200);
+
+        });
+
+        this.el.addEventListener("click", () => {
+            this.el.setAttribute("animation-mixer", "timeScale: 0.2;");
             this.el.addEventListener("animation-loop", () => {
                 this.el.removeAttribute("animation-mixer");
             })
-            
         });
-
-        this.el.addEventListener("raycaster-intersected-cleared", () => {
-            this.el.removeAttribute("animation-mixer");
-        })
-        // this.el.addEventListener("click", () => {
-        //     this.el.setAttribute("animation-mixer", "clip: L_door|open_left; repeat: once"); 
-        //     this.el.removeAttribute("animation-mixer");
-        // });
 	}
 })
