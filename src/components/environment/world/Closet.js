1e58b7a330025b21e546a800b486c3149f4ce099
niels_desktop <niels.hoogeboom@student.hu.nl>
2023-05-01 20:23:18 +0200

    sounds toegevoegd voor verschillend opening states

--- src/components/environment/world/Closet.js
+++ src/components/environment/world/Closet.js
@@ -1,22 +1,25 @@
 AFRAME.registerComponent("own-closet", {
 	init: function () {
         this.el.setAttribute("class", "interactable");
-        this.el.setAttribute("sound", "src: #kast;")
 
         this.el.addEventListener("raycaster-intersected", () => {
+            this.el.setAttribute("sound", "src: #kast;")
             this.el.components.sound.playSound();
         })
         this.el.addEventListener("click", () => {
-            console.log("ik ben een coole kast");
+            this.el.setAttribute("sound", "src: #kastopen;")
             this.el.setAttribute("animation-mixer", "clip: 001; timeScale: 0.2;");
             setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1200);
+            this.el.components.sound.playSound();
 
         });
 
         this.el.addEventListener("click", () => {
             this.el.setAttribute("animation-mixer", "timeScale: 0.2;");
             this.el.addEventListener("animation-loop", () => {
+                this.el.setAttribute("sound", "src: #kastdicht;")
                 this.el.removeAttribute("animation-mixer");
+                this.el.components.sound.playSound();
             })
         });
 	}
