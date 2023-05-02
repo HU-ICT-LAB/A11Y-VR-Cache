93168d9d5205da6957d153f2db0864f6bf0199c5
niels_desktop <niels.hoogeboom@student.hu.nl>
2023-05-02 20:04:22 +0200

    cache klaar gezet ook op de 2de versie zodat er 2 versies gespeeld kunnen worden

--- src/components/environment/world/Closet.js
+++ src/components/environment/world/Closet.js
@@ -1,4 +1,6 @@
 AFRAME.registerComponent("own-closet", {
+    closetOpened: false,
+
 	init: function () {
         this.el.setAttribute("class", "interactable");
         this.el.setAttribute("sound", "src: #kast;")
@@ -12,13 +14,17 @@ AFRAME.registerComponent("own-closet", {
             this.el.components.sound.playSound();
         });
 
-        this.el.addEventListener("click", () => {
-            this.el.setAttribute("sound", "src: #kastopen;")
-            this.el.setAttribute("animation-mixer", "clip: 001; timeScale: 0.2;");
-            setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1200);
-            this.el.components.sound.playSound();
-
-        });
+        if(!(this.closetOpened)) {
+            this.el.addEventListener("click", () => {
+                this.el.setAttribute("sound", "src: #kastopen;")
+                this.el.setAttribute("animation-mixer", "clip: 001; timeScale: 0.2;");
+                setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1200);
+                this.el.components.sound.playSound();
+                this.closetOpened = true;
+                document.getElementById("cache").setAttribute("sound", "src: #dichtbijCache; autoplay: true; loop: true; rolloffFactor: 40");
+    
+            });
+        }
 
         this.el.addEventListener("click", () => {
             this.el.setAttribute("animation-mixer", "timeScale: 0.2;");
@@ -26,7 +32,9 @@ AFRAME.registerComponent("own-closet", {
                 this.el.setAttribute("sound", "src: #kastdicht;")
                 this.el.removeAttribute("animation-mixer");
                 this.el.components.sound.playSound();
+                document.getElementById("cache").removeAttribute("sound", "src: #dichtbijCache; autoplay: true; loop: true; rolloffFactor: 40");
             })
+            this.closetOpened = false;
         });
 	}
 })
