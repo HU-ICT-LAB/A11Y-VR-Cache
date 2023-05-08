e27ac39dcc07d3e97b8a2c33b844a22319b308dc
niels <niels.hoogeboom@student.hu.nl>
2023-05-08 10:58:34 +0200

    kast animatie gefixt

--- src/components/environment/world/Closet.js
+++ src/components/environment/world/Closet.js
@@ -9,27 +9,29 @@ AFRAME.registerComponent("own-closet", {
             this.el.components.sound.playSound();
         })
 
-        if(!(this.closetState)) {
+        if(this.closetOpened === false) {
             this.el.addEventListener("click", () => {
                 this.el.setAttribute("sound", "src: #kastopen;")
                 this.el.setAttribute("animation-mixer", "clip: 001; timeScale: 0.2;");
-                setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1200);
+                setTimeout(() => {this.el.setAttribute("animation-mixer", "timeScale: 0;")}, 1600);
                 this.el.components.sound.playSound();
                 this.closetOpened = true;
                 document.getElementById("cache").setAttribute( "sound", "src: #dichtbijCache; autoplay: true; loop: true; rolloffFactor: 40");
     
             });
+        }else {
+            this.el.addEventListener("click", () => {
+                this.el.setAttribute("animation-mixer", "timeScale: 0.2;");
+                this.el.addEventListener("animation-loop", () => {
+                    this.el.setAttribute("sound", "src: #kastdicht;")
+                    this.el.removeAttribute("animation-mixer");
+                    this.el.components.sound.playSound();
+                    document.getElementById("cache").removeAttribute("sound");
+                    this.closetOpened = false;
+                })
+            });
         }
-        this.el.addEventListener("click", () => {
-            this.el.setAttribute("animation-mixer", "timeScale: 0.2;");
-            this.el.addEventListener("animation-loop", () => {
-                this.el.setAttribute("sound", "src: #kastdicht;")
-                this.el.removeAttribute("animation-mixer");
-                this.el.components.sound.playSound();
-                document.getElementById("cache").removeAttribute( "sound");
-            })
-            this.closetOpened = false;
-        });
+   
 	}
 })
 
