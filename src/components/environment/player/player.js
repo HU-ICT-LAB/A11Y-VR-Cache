291026bcc4e98b3495e38d3c05c60e41d7bb51dc
niels_desktop <niels.hoogeboom@student.hu.nl>
2023-05-16 20:25:51 +0200

    Update zonder reload

--- src/components/environment/player/player.js
+++ src/components/environment/player/player.js
@@ -1,49 +1,57 @@
 AFRAME.registerComponent("player", {
+	schema: {
+		versie: {type: "string"}
+	},
+
+	 versie1: function(e) {
+		console.log(e.detail.els[0]);
+		let object = e.detail.els[0];
+			object.components.sound.playSound();
+	
+	
+	},
+
+	versie2: function() {
+		console.log("ik ben aan het trillen");
+			document.getElementById("right").components.haptics.pulse(1, 50);
+	},
+
+	clickSound: function(e) {
+		let object = e.detail.intersectedEl;
+		object.components.sound.playSound();
+	},
+
 	init: function () {
 		const element = this.el;
-
-		if(sessionStorage.getItem("versie") === "1") {
-			let object = sessionStorage.getItem("object");
-			switch(object) {
-				case "bed":  
-			}
-		}
-
 		element.addEventListener("bButtonDown", () => {
 				console.log("enter toets werkt")
-				if(sessionStorage.getItem("versie") === "1") {
-					sessionStorage.setItem("versie",  2);
-					window.location.reload();
+				if(this.data.versie === "1") {
+					element.setAttribute("player", "versie: 2");
+					console.log(this.data.versie);
 					return;
 				}
-				sessionStorage.setItem("versie",  1);
-				window.location.reload();
+				element.setAttribute("player", "versie: 1");
+				console.log(this.data.versie);
 			});
-
 		
-		if(sessionStorage.getItem("versie") === "1") {
-				element.addEventListener("raycaster-intersection", (e) => {
-					let object = e.detail.els[0];
-					object.components.sound.playSound();
-				
-				});
+	},
+
+	update: function(oldData) {
+		const element = this.el;
+
+		element.addEventListener("raycaster-intersection", this.versie1);
+
+		if(this.data.versie === "1") {
+			element.removeEventListener("raycaster-intersection", this.versie2);
+			element.removeEventListener("click", this.clickSound);
+			element.addEventListener("raycaster-intersection", this.versie1)
 		}
 
 		else {
-			element.addEventListener("raycaster-intersection", () => {
-				console.log("ik ben aan het trillen");
-				element.components.haptics.pulse(1, 50);
-			
-			});
-
-			element.addEventListener("click", (e) => {
-				let object = e.detail.intersectedEl;
-				object.components.sound.playSound();
-			})
+			element.removeEventListener("raycaster-intersection", this.versie1);
+			element.addEventListener("raycaster-intersection", this.versie2);
+			element.addEventListener("click", this.clickSound);
 		}
-		
 
-
-		
 	}
 })
