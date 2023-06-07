61495104657d6490a64933fdf4c6d2d021dc4f31
NielsStudent <niels.hoogeboom@student.hu.nl>
2023-06-07 13:16:01 +0200

    knop veranderd naar x button

--- src/components/environment/player/player.js
+++ src/components/environment/player/player.js
@@ -24,7 +24,7 @@ AFRAME.registerComponent("player", {
 	init: function () {
 		const element = this.el;
 
-		element.addEventListener("bbuttonup", () => {
+		element.addEventListener("xbuttonup", () => {
 				console.log("enter toets werkt")
 				if(this.data.versie === "1") {
 					element.setAttribute("player", "versie: 2");
