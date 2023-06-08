0741de9c320b9e74312f950a357e1893f3ba378a
astraHU <astra.hedvat@student.hu.nl>
2023-06-08 20:02:37 +0200

    sleutel functie gefixd

--- src/components/environment/world/Key.js
+++ src/components/environment/world/Key.js
@@ -1,10 +1,22 @@
 AFRAME.registerComponent("key", {
-    
-	init: function () {
-        const el = this.el;
-        el.setAttribute("class", "interactable");
-        el.setAttribute("id", "key");
-        el.setAttribute("sound", "src: #keySound");
-   
-	}
+        schema: {
+                keyFound: { type: "string" }
+        },
+
+        init: function () {
+                const el = this.el;
+                el.setAttribute("class", "interactable");
+                this.el.setAttribute("key", "keyFound: false");
+                el.setAttribute("id", "key");
+                el.setAttribute("sound", "src: #keySound");
+
+        },
+
+        //when clicked -> keyFound == true
+        //if keyFound == true -> key disappears
+
+        update: function (oldData) {
+                this.el.addEventListener("click", () => { this.el.setAttribute("key", "keyFound: true"
+                ) })
+        }
 })
