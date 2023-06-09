d3e9b479fd617636152247f9b1d1c2ba57e7ab0a
niels <niels.hoogeboom@student.hu.nl>
2023-06-09 10:14:55 +0200

    knop veranderd voor testen

--- src/components/environment/world/Key.js
+++ src/components/environment/world/Key.js
@@ -9,7 +9,7 @@ AFRAME.registerComponent("key", {
                 el.setAttribute("id", "key");
                 el.setAttribute( "sound", "src: #dichtbijCache; autoplay: true; loop: true; rolloffFactor: 40");
 
-                this.el.addEventListener("gripup", () => {
+                this.el.addEventListener("click", () => {
                         sessionStorage.setItem("keyFound", "true")
                         this.el.setAttribute("key", "keyFound: true")
         })
