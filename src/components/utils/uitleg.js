d505f988c1498d86b8d3c0fbc70f86d87a70ecca
Julie Gunning <juulgunning@gmail.com>
2023-06-27 19:03:56 +0200

    test

--- src/components/utils/uitleg.js
+++ src/components/utils/uitleg.js
@@ -4,13 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   
     uitlegAnchor.addEventListener("click", function(e) {
       e.preventDefault();
-      
-      // Check if the media element is paused or ended before playing
-      if (uitleg.paused || uitleg.ended) {
-        uitleg.play();
-      } else {
-        // If the media element is already playing, pause it
-        uitleg.pause();
-      }
+      uitleg.play();
     });
   });
