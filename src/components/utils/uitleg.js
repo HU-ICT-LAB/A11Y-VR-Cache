5b4657d6cb94d2e1992db88abf19fd4da6af4b0c
Julie Gunning <juulgunning@gmail.com>
2023-06-27 18:54:15 +0200

    test on meta quest browser

--- src/components/utils/uitleg.js
+++ src/components/utils/uitleg.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("uitlegAnchor").addEventListener("click", function(e){
-    e.preventDefault();
-    document.getElementById("uitleg").play();
-    })
-}
-)
+    var uitlegAnchor = document.getElementById("uitlegAnchor");
+    var uitleg = document.getElementById("uitleg");
+  
+    uitlegAnchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      
+      // Check if the media element is paused or ended before playing
+      if (uitleg.paused || uitleg.ended) {
+        uitleg.play();
+      } else {
+        // If the media element is already playing, pause it
+        uitleg.pause();
+      }
+    });
+  });
